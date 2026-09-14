81deeb61785fc9a9241cc180b20401e69dcf6999
virtualeconomist <87089693+virtualeconomist@users.noreply.github.com>
2026-09-14 16:06:20 -0700

    Expose Sprite Loop renderer API

---
+++ src/stella/sprite/index.ts
@@ -0,0 +1,17 @@
+export {
+  availableSpriteExpressions,
+  spriteAsset,
+  spriteVariant,
+  validateSpriteAssetPack
+} from './manifest'
+export type { SpriteAssetPack, SpriteAssetVariant } from './manifest'
+export {
+  createSpriteSequenceRenderer,
+  spriteFrameForSequence
+} from './sequence-renderer'
+export type { SpriteSequenceFrame } from './sequence-renderer'
+export {
+  SPRITE_ASSET_PACKS,
+  STELLA_SPRITE_PACK,
+  spriteAssetPack
+} from './stella-pack'
