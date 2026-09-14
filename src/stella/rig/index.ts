b25d1e6a3b847e45e3b824408bab25f29f9b8fd0
virtualeconomist <87089693+virtualeconomist@users.noreply.github.com>
2026-09-14 03:30:42 -0700

    Expose modular face rig foundation

---
+++ src/stella/rig/index.ts
@@ -0,0 +1,15 @@
+export { STELLA_EXPRESSION_PRESETS, getStellaExpressionPreset } from './expression-presets'
+export { clamp01, interpolateFaceRigState, lerp } from './interpolate'
+export { STELLA_RIG_MANIFEST } from './stella-manifest'
+export type {
+  BlushRigState,
+  BrowRigState,
+  CharacterRigManifest,
+  ExpressionEffectsState,
+  EyeRigState,
+  EyeShapeWeights,
+  FaceRigAnchors,
+  FaceRigState,
+  MouthRigState,
+  Vec2
+} from './types'
