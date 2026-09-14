072f33714dc68fc7be08daff548a1b4ca02106ac
virtualeconomist <87089693+virtualeconomist@users.noreply.github.com>
2026-09-14 01:01:52 -0700

    Add explicit free-motion cursor follow toggle

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
+import { installFreeMotionToggle } from './free-motion'
 
 createApp(App).mount('#app')
+installFreeMotionToggle()
