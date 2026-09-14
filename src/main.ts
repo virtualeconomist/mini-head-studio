2dcfdcbd2faaea8a5e9aefc9cf6c9237d26e50f0
virtualeconomist <87089693+virtualeconomist@users.noreply.github.com>
2026-09-14 03:07:20 -0700

    Enable page-wide free motion and expression loops

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
 import { installFreeMotionToggle } from './free-motion'
+import { installExpressionLoop } from './expression-loop'
 
 createApp(App).mount('#app')
 installFreeMotionToggle()
+installExpressionLoop()
